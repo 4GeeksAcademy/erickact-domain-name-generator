e94d993329eb922ac3fbfca2f776c9503bc5d6f1
Ericka Castillo <84164725+erickact@users.noreply.github.com>
2024-07-31 16:15:19 +0000

    add some extensions

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 let pronoun = ['the', 'our'];
 let adj = ['great', 'big'];
 let noun = ['jogger', 'racoon'];
+let extens = [".com", ".net", ".us", ".io"]
 
 for (let i of pronoun) {
     for (let j of adj) {
       for (let k of noun) {
-        console.log(i + j + k + '.com');
+        for (let l of extens) {
+          console.log(i + j + k + l);
+        }
       }
     }
   }
