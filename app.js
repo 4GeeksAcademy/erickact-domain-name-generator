977ab4d5f46c720cdfae611f6329f6ccdb507cf1
Ericka Castillo <84164725+erickact@users.noreply.github.com>
2024-07-31 15:23:57 +0000

    domain-name-generator

---
+++ app.js
@@ -0,0 +1,15 @@
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+
+for (let i of pronoun) {
+    for (let j of adj) {
+      for (let k of noun) {
+        console.log(i + j + k + '.com');
+      }
+    }
+  }
+
+
+
+
